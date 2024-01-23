e6ab95a8ad532440779cc9dae9db995de24ba02e
psylo <psylion@gmail.com>
2024-01-23 18:47:03 +0100

    new web ongoing

--- psylo.net/gist.php
+++ psylo.net/gist.php
@@ -6,6 +6,7 @@
  */
 function getMp3StreamTitle($streamingUrl, $interval, $offset = 0, $headers = true)
 {
+	var_dump($streamingUrl);
 	$needle = 'StreamTitle=';
 	$ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36';
 
@@ -18,7 +19,10 @@ function getMp3StreamTitle($streamingUrl, $interval, $offset = 0, $headers = tru
 	];
 
 	if (($headers = get_headers($streamingUrl))) {
+		var_dump($header); 
+		echo"\n\n";
 		foreach ($headers as $h) {
+			var_dump($h);
 			if (strpos(strtolower($h), 'icy-metaint') !== false && ($interval = explode(':', $h)[1])) {
 				break;
 			}
@@ -30,7 +34,6 @@ function getMp3StreamTitle($streamingUrl, $interval, $offset = 0, $headers = tru
 	if ($stream = fopen($streamingUrl, 'r', false, $context)) {
 		$buffer = stream_get_contents($stream, $interval, $offset);
 		fclose($stream);
-
 		if (strpos($buffer, $needle) !== false) {
 			$title = explode($needle, $buffer)[1];
 			return substr($title, 1, strpos($title, ';') - 2);
@@ -41,7 +44,12 @@ function getMp3StreamTitle($streamingUrl, $interval, $offset = 0, $headers = tru
 		throw new Exception("Unable to open stream [{$streamingUrl}]");
 	}
 }
+echo"\n\n";
 
-var_dump(getMp3StreamTitle('https://pbbradio.com:8443/128', 19200));
+var_dump(getMp3StreamTitle('http://psylo.ddns.net:8000/PGS1', 19200));
+echo "\n\n";
+var_dump(getMp3StreamTitle('http://psylo.ddns.net:8000/PGS2', 19200));
+echo "\n\n";
+var_dump(getMp3StreamTitle('http://psylo.ddns.net:8000/PGS3', 19200));
 
 echo "\n\n";
